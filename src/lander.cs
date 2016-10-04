17e04d5639c6c31329baba2cb47f2a2ecee95c8b
Jason Stanton <Jason Stanton>
2016-10-04 15:41:11 +1100

    Created Visual Studio Project and basic classes.

---
+++ src/lander.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwinGameSDK;
+
+namespace lander
+{
+    /// <summary>
+    /// This lander class is responsible for all aspects of the lander such as its movement and behaviour.
+    /// </summary>
+    class lander
+    {
+        double mass;
+        double veloctiy;
+        double gravity;
+        Vector direction;
+
+        /// <summary>
+        /// Method for drawing the lander on screen.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        public void DrawLander(int x, int y)
+        {
+
+        }
+
+
+    }
+}
---
+++ src/landingPad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SwinGameSDK;
+
+namespace lander
+{
+    class landingPad
+    {
+        /// <summary>
+        /// Method to draw the landing pad.
+        /// </summary>
+        public void DrawLandingPad()
+        {
+
+        }
+
+        public void DetectCollision()
+        {
+
+        }
+    }
+}

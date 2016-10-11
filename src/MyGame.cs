4b07261dc1d063c6aa569af3549922748b8898b5
Jason Stanton <Jason Stanton>
2016-10-11 13:41:28 +1100

    Added gravity and basic thrust logic for lander

--- src/MyGame.cs
+++ src/MyGame.cs
@@ -3,14 +3,16 @@ using SwinGameSDK;
 
 namespace lander
 {
-    public class GameMain
+    public class MyGame
     {
         public static void Main()
         {
             //Open the game window
-            SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
+            SwinGame.OpenGraphicsWindow("GameMain", 1600, 900);
             SwinGame.ShowSwinGameSplashScreen();
-            
+
+            Lander l = new Lander((SwinGame.ScreenWidth() / 2) - (30 / 2), 10); // 30 is lander bitmap width hardcoded
+
             //Run the game loop
             while(false == SwinGame.WindowCloseRequested())
             {
@@ -20,10 +22,15 @@ namespace lander
                 //Clear the screen and draw the framerate
                 SwinGame.ClearScreen(Color.White);
                 SwinGame.DrawFramerate(0,0);
-                
+
+                l.CalculatePosition(); // Move the lander
+                l.DrawLander(); // Draw the lander
+
                 //Draw onto the screen
                 SwinGame.RefreshScreen(60);
             }
+            SwinGame.ReleaseAllResources();
+            SwinGame.ReleaseAllSprites();
         }
     }
 }
--- src/lander.cs
+++ src/lander.cs
@@ -4,29 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SwinGameSDK;
+using System.Diagnostics;
 
 namespace lander
 {
     /// <summary>
     /// This lander class is responsible for all aspects of the lander such as its movement and behaviour.
     /// </summary>
-    class lander
+    class Lander
     {
-        double mass;
-        double veloctiy;
-        double gravity;
-        Vector direction;
+        Sprite _lander = SwinGame.CreateSprite(SwinGame.LoadBitmapNamed("lander", "Resources/images/lander.png"));
 
-        /// <summary>
-        /// Method for drawing the lander on screen.
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        public void DrawLander(int x, int y)
+        public Lander(int x, int y)
         {
-
+            _lander.X = x;
+            _lander.Y = y;
         }
 
+        /// <summary>
+        /// Draw the lander on the screen.
+        /// </summary>
+        /// <param name="x">Set X position for lander to be drawn.</param>
+        /// <param name="y">Set Y position for lander to be drawn.</param>
+        public void DrawLander(int x, int y)
+        {
+            SwinGame.SpriteSetX(_lander, x);
+            SwinGame.SpriteSetY(_lander, y);
 
+            SwinGame.UpdateSprite(_lander);
+            SwinGame.DrawSprite(_lander);
+        }
+
+        /// <summary>
+        /// Draw the lander on the screen.
+        /// </summary>
+        public void DrawLander()
+        {
+            SwinGame.UpdateSprite(_lander);
+            SwinGame.DrawSprite(_lander);
+        }
+
+        /// <summary>
+        /// Read input from user and update lander position accordingly.
+        /// </summary>
+        public void CalculatePosition()
+        {
+            Vector thrustVector = new Vector();
+            Vector gravityVector = new Vector();
+            thrustVector.Y = (float)-0.1;
+            gravityVector.Y = (float)0.05;
+
+            if (SwinGame.KeyDown(KeyCode.vk_LEFT))
+            {
+                thrustVector.X = -1;
+                _lander.AddToVelocity(thrustVector);
+                _lander.Rotation = thrustVector.X;
+            }
+            else if (SwinGame.KeyDown(KeyCode.vk_RIGHT))
+            {
+                thrustVector.X = 1;
+                _lander.AddToVelocity(thrustVector);
+                _lander.Rotation = thrustVector.X;
+            }
+
+            else if (SwinGame.KeyDown(KeyCode.vk_UP))
+            {
+                _lander.AddToVelocity(thrustVector);
+                SwinGame.MoveSprite(_lander);
+
+            }
+            _lander.AddToVelocity(gravityVector);
+        }
     }
 }
